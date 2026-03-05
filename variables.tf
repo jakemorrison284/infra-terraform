dc625b25001f4744172279da3b2edd81b0db6c5f
jakemorrison284 <jakemorrison@walla.co.il>
2026-03-05 16:30:44 +0200

    Add variables.tf for VPC module

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment for the VPC"
+  type        = string
+}
