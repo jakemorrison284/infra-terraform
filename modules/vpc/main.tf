3b09aac68cb9344a211ffc18bd65dca2f7195fc5
jakemorrison284 <jakemorrison@walla.co.il>
2026-04-21 15:14:11 +0300

    Implement Redis cluster upgrade configurations

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,3 +10,27 @@ resource "aws_subnet" "private" {
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
 }
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-cluster"
+  engine              = "redis"
+  engine_version      = "Y.Y.Y" # Update to the desired Redis version
+  node_type           = "cache.t2.micro" # Update the instance type as needed
+  number_cache_nodes   = 3 # Configure cluster size for high availability
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids   = [aws_security_group.redis_sg.id]
+
+  tags = {
+    Name        = "Redis Cluster"
+    Environment = var.environment
+  }
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "redis-subnet-group"
+  subnet_ids = var.private_subnets
+
+  tags = {
+    Name = "Redis Subnet Group"
+  }
+}
