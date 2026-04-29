d217dac529641d6a14b54215f801c316e82e59ae
jakemorrison284 <jakemorrison@walla.co.il>
2026-04-29 19:18:22 +0300

    Implemented validations and expanded documentation for VPC module variables.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -15,13 +15,21 @@ variable "public_subnets_count" {
 }
 
 variable "private_subnets_count" {
-  description = "Number of private subnets"
+  description = "Number of private subnets (must be greater than 0)"
   type        = number
   default     = 1
+  validation {
+    condition     = (var.private_subnets_count > 0)
+    error_message = "The number of private subnets must be greater than 0."
+  }
 }
 
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC (should follow CIDR notation)"
+  description = "CIDR block for the VPC (should follow CIDR notation and not overlap with existing CIDR blocks)"
   type        = string
   default     = "10.0.0.0/22"
+  validation {
+    condition     = can(regex("^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$", var.vpc_cidr_block))
+    error_message = "The CIDR block format is invalid. Please use CIDR notation (e.g., 10.0.0.0/22)."
+  }
 }
