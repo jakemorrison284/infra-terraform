751e78048f79895120e083514590bc3ff2036f3a
jakemorrison284 <jakemorrison@walla.co.il>
2026-04-29 18:37:23 +0300

    Updated variables.tf to reduce over-provisioned resources in the VPC module.

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,83 +3,20 @@ variable "private_subnets" {
   default = ["10.0.3.0/24"] # Adjusted to a single smaller CIDR block
 }
 
-resource "aws_vpc" "novapay" {
-  cidr_block           = "10.0.0.0/22"  # Reduced CIDR block to save IP space
-  enable_dns_hostnames = true
-  tags = { Name = "novapay-vpc", Environment = var.environment }
+variable "public_subnets_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2  # Reduced count to save costs
 }
 
-# Public Subnets
-resource "aws_subnet" "public" {
-  count             = 2  # Reduced count to save costs
-  vpc_id            = aws_vpc.novapay.id
-  cidr_block        = ["10.0.1.0/25", "10.0.1.128/25"][count.index]
-  availability_zone = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+variable "private_subnets_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 1  # Reduced count to save costs
 }
 
-# Private Subnets
-resource "aws_subnet" "private" {
-  count             = 1  # Reduced count to save costs
-  vpc_id            = aws_vpc.novapay.id
-  cidr_block        = var.private_subnets[count.index]
-  availability_zone = var.availability_zones[count.index]
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.novapay.id
-}
-
-# Route Table for Public Subnets
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.novapay.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# Associate Public Subnets with Route Table
-resource "aws_route_table_association" "public" {
-  count          = 2  # Updated count to match the number of public subnets
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-# NAT Gateway for Private Subnets
-resource "aws_nat_gateway" "nat" {
-  count       = 1  # Reduced to a single NAT Gateway
-  allocation_id = aws_eip.nat.id
-  subnet_id    = aws_subnet.public[0].id
-}
-
-resource "aws_eip" "nat" {
-  count = 1  # Only one EIP for the NAT Gateway
-  vpc = true
-}
-
-# Route Table for Private Subnets
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.novapay.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id  # Updated to use NAT Gateway
-  }
-}
-
-# Associate Private Subnets with Route Table
-resource "aws_route_table_association" "private" {
-  count          = 1  # Updated count to match the number of private subnets
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
-
-# Enable VPC Flow Logs with filtering
-resource "aws_flow_log" "vpc_flow_logs" {
-  log_group_name = "vpc-flow-logs"
-  traffic_type   = "ACCEPT"  # Changed to log only accepted traffic
-  vpc_id         = aws_vpc.novapay.id
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/22"  # Adjusted CIDR block for efficiency
 }
