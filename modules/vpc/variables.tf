6c14dbc78f85fc9f0ea4ee1a1d6d42954d3b97d3
jakemorrison284 <jakemorrison@walla.co.il>
2026-04-29 19:10:14 +0300

    Enhanced variable contracts for the VPC module in variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,22 +1,27 @@
 variable "private_subnets" {
-  type    = list(string)
-  default = ["10.0.3.0/24"] # Adjusted to a single smaller CIDR block
+  type        = list(string)
+  description = "List of CIDR blocks for private subnets"
+  default     = ["10.0.3.0/24"] 
 }
 
 variable "public_subnets_count" {
-  description = "Number of public subnets"
+  description = "Number of public subnets (must be between 1 and 5)"
   type        = number
-  default     = 2  # Reduced count to save costs
+  default     = 2
+  validation {
+    condition     = (var.public_subnets_count >= 1 && var.public_subnets_count <= 5)
+    error_message = "The number of public subnets must be between 1 and 5."
+  }
 }
 
 variable "private_subnets_count" {
   description = "Number of private subnets"
   type        = number
-  default     = 1  # Reduced count to save costs
+  default     = 1
 }
 
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for the VPC (should follow CIDR notation)"
   type        = string
-  default     = "10.0.0.0/22"  # Adjusted CIDR block for efficiency
+  default     = "10.0.0.0/22"
 }
